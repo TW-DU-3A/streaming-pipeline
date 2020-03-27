9873d20bc6782fce52a04c4255b99a5b76f9db4f
in-sabyasachi.nandy <sabyasachi.nandy@thoughtworks.com>
2020-03-27 15:55:09 +0530

    Neel/Manish - Fix MessageMetadataTest and add properties for test.

--- zookeeper/seed.sh
+++ zookeeper/seed.sh
@@ -3,9 +3,17 @@ echo $zk_command
 $zk_command rmr /tw
 $zk_command create /tw ''
 
+#TODO Check the NYC topic creation, no dataLocation specified.
 $zk_command create /tw/stationDataNYC ''
 $zk_command create /tw/stationDataNYC/topic station_data_nyc
 $zk_command create /tw/stationDataNYC/checkpointLocation hdfs://$hdfs_server/tw/rawData/stationDataNYC/checkpoints
+
+$zk_command create /tw/stationDataSF ''
+$zk_command create /tw/stationDataSF/kafkaBrokers $kafka_server
+$zk_command create /tw/stationDataSF/topic station_data_sf
+$zk_command create /tw/stationDataSF/checkpointLocation hdfs://$hdfs_server/tw/rawData/stationDataSF/checkpoints
+$zk_command create /tw/stationDataSF/dataLocation hdfs://$hdfs_server/tw/rawData/stationDataSF/data
+
 
 $zk_command create /tw/stationInformation ''
 $zk_command create /tw/stationInformation/kafkaBrokers $kafka_server
@@ -19,11 +27,6 @@ $zk_command create /tw/stationStatus/topic station_status
 $zk_command create /tw/stationStatus/checkpointLocation hdfs://$hdfs_server/tw/rawData/stationStatus/checkpoints
 $zk_command create /tw/stationStatus/dataLocation hdfs://$hdfs_server/tw/rawData/stationStatus/data
 
-$zk_command create /tw/stationDataSF ''
-$zk_command create /tw/stationDataSF/kafkaBrokers $kafka_server
-$zk_command create /tw/stationDataSF/topic station_data_sf
-$zk_command create /tw/stationDataSF/checkpointLocation hdfs://$hdfs_server/tw/rawData/stationDataSF/checkpoints
-$zk_command create /tw/stationDataSF/dataLocation hdfs://$hdfs_server/tw/rawData/stationDataSF/data
 
 $zk_command create /tw/stationDataFR ''
 $zk_command create /tw/stationDataFR/kafkaBrokers $kafka_server
