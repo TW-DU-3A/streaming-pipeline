fd523638d947cc517a48e43183d2a81bfdeb33c7
in-sabyasachi.nandy <sabyasachi.nandy@thoughtworks.com>
2020-03-30 10:38:39 +0530

    Neel/Manish - Add separate docker compose for E2E testing with docker airflow.

--- CitibikeApiProducer/src/main/java/com/tw/Application.java
+++ CitibikeApiProducer/src/main/java/com/tw/Application.java
@@ -7,7 +7,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.concurrent.CountDownLatch;
