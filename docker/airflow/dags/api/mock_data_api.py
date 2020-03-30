aadb9c627868da7a1f08913fa8c765f335a8febc
in-sabyasachi.nandy <sabyasachi.nandy@thoughtworks.com>
2020-03-30 14:38:38 +0530

    Neel/Manish | 10 | - Add dag with basic task to run a mock api server.

---
+++ docker/airflow/dags/api/mock_data_api.py
@@ -0,0 +1,21 @@
+from flask import Flask, Response, abort
+import os
+from pathlib import Path
+import json
+from flask import jsonify
+
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+FILE_PATH = os.path.join(os.path.join(BASE_DIR, "resources"), "sf-mock-response.json")
+app = Flask(__name__)
+
+
+@app.route('/mock-data')
+def mock_data():
+    with open(FILE_PATH) as file:
+        data = json.load(file)
+        return jsonify(data)
+
+
+def api_server():
+    app.run()
---
+++ docker/airflow/dags/e2e.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+
+import os
+import sys
+from airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+
+sys.path.insert(0,os.path.abspath(os.path.dirname("api")))
+from api import mock_data_api
+
+DAG_ID = os.path.basename(__file__).replace(".pyc", "").replace(".py", "")
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'retries': 0,
+}
+
+dag = DAG(DAG_ID, default_args=default_args, schedule_interval=None,
+          start_date=(datetime(2020, 3, 16, 0, 0, 0, 0)), catchup=False)
+
+mock_data_api = PythonOperator(task_id="mock_data_api", python_callable=mock_data_api.api_server, dag=dag)
+
+
