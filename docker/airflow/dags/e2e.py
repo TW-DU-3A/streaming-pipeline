857d9b9d26c8b080dcffc02ed8dd11d669051d0c
Vivek Soni <erviveksoni@gmail.com>
2020-04-03 12:29:32 +0530

    Vivek/Swathi | 10 | ADD | REMOVE | Added the dag task to compare mock data with xformation data. Updated gradle version

--- docker/airflow/dags/e2e.py
+++ docker/airflow/dags/e2e.py
@@ -1,12 +1,18 @@
 from __future__ import print_function
-
-import os
-import sys
 from airflow.models import DAG
 from datetime import datetime
 from airflow.sensors import WebHdfsSensor
+from airflow.operators.python_operator import PythonOperator
+from hdfs import client
+import pandas as pd
+import sys, os
+import tempfile
+import glob
+import requests
+from io import StringIO
+from airflow.hooks.base_hook import BaseHook
 
-sys.path.insert(0,os.path.abspath(os.path.dirname("api")))
+sys.path.insert(0, os.path.abspath(os.path.dirname("api")))
 
 DAG_ID = os.path.basename(__file__).replace(".pyc", "").replace(".py", "")
 
@@ -25,3 +31,58 @@ file_sensor = WebHdfsSensor(
     filepath=input_path,
     webhdfs_conn_id='webhdfs_default',
     dag=dag)
+
+
+def read_hdfs_file():
+    connection = BaseHook.get_connection("webhdfs_default")
+    conn_type = connection.conn_type
+    host = connection.host
+    port = connection.port
+    hdfs_conn = str(conn_type) + "://" + str(host) + ":" + str(port)
+    print("connecting to " + hdfs_conn)
+    myclient = client.Client(hdfs_conn)
+    pathname = os.path.join(tempfile.mkdtemp(), 'hdfs_stationMart_data')
+    print("downloading files from HDFS to " + pathname)
+    myclient.download("/tw/stationMart/data/", pathname, True)
+    print("downloading complete")
+    all_files = glob.glob(pathname + "/*.csv")
+    if len(all_files) > 0 and os.path.getsize(all_files[0]) > 0:
+        actual_df = pd.read_csv(all_files[0])
+        print(actual_df)
+        return actual_df
+    print("No files downloaded..")
+    return None
+
+
+def read_mock_data():
+    print("Fetching mock response text...")
+    connection = BaseHook.get_connection("MOCKSERVER")
+    conn_type = connection.conn_type
+    host = connection.host
+    port = connection.port
+    mockserver_conn = str(conn_type) + "://" + str(host) + ":" + str(port) + "/" + "mock-response"
+    print("connecting to " + mockserver_conn)
+    r = requests.get(mockserver_conn)
+    StringData = StringIO(r.text)
+    df = pd.read_csv(StringData, sep=",")
+    return df
+
+
+def compare_data(**kwargs):
+    mock_df = read_mock_data()
+    actual_df = read_hdfs_file()
+    result = actual_df.equals(mock_df)
+    print("Result: " + str(result))
+    if not result:
+        raise ValueError('mart data did not match with reference data!')
+    return result
+
+
+comparison_task = PythonOperator(
+    task_id='compare_mock_with_martdata'
+    , python_callable=compare_data
+    , provide_context=True
+    , dag=dag
+)
+
+file_sensor >> comparison_task
--- docker/mock_server/mock_data_api.py
+++ docker/mock_server/mock_data_api.py
@@ -8,6 +8,8 @@ from flask import Flask
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent
 FILE_PATH = os.path.join(os.path.join(BASE_DIR, "resources"), "sf-mock-response.json")
+OUTPUT_FILE_PATH = os.path.join(os.path.join(BASE_DIR, "resources"), "mock_stationMart_1.csv")
+
 app = Flask(__name__)
 
 @app.route('/mock-data')
@@ -16,6 +18,11 @@ def mock_data():
         data = json.load(file)
         return jsonify(data)
 
+@app.route('/mock-response')
+def mock_response():
+    with open(OUTPUT_FILE_PATH) as file:
+        contents = file.read()
+        return Response(contents, content_type='text/plain; charset=utf-8')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
